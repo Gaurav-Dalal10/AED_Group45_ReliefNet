b89775425810d7ae050a6114bf1981925d24a440
Gaurav-Dalal10 <dalal.g@northeastern.edu>
2024-12-08 03:53:37 -0500

    Updated classes in model

--- ReliefNet/src/Model/FoodCare.java
+++ ReliefNet/src/Model/FoodCare.java
@@ -4,10 +4,54 @@
  */
 package Model;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 /**
  *
  * @author Hp
  */
 public class FoodCare {
+    
+    private String food_name;
+    private int food_quantity;
+    private LocalDate ordDt;
+
+    public String getFood_name() {
+        return food_name;
+    }
+
+    public void setFood_name(String food_name) {
+        this.food_name = food_name;
+    }
+
+    public int getFood_quantity() {
+        return food_quantity;
+    }
+
+    public void setFood_quantity(int food_quantity) {
+        this.food_quantity = food_quantity;
+    }
+
+    public LocalDate getOrdDt() {
+        return ordDt;
+    }
+
+    public void setOrdDt(LocalDate ordDt) {
+        this.ordDt = ordDt;
+    }
+    
+            
+    public FoodCare(String name, int quantity, LocalDate ordDate){
+        this.food_name = name;
+        this.food_quantity = quantity;
+        this.ordDt = ordDate;
+    }
+    
+        
+    @Override
+    public String toString(){
+        return food_name;
+    }
     
 }
