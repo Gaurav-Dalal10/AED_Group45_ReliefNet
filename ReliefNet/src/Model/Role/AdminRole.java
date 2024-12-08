ae9c7ef25512e88f6c18fcda9e7e00007df38ecd
aditi240198 <deshmukh.adi@northeastern.edu>
2024-12-08 05:47:54 -0500

    Model Roles

---
+++ ReliefNet/src/Model/Role/AdminRole.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Role;
+
+import Model.EcoSystem;
+import Model.Network.Network;
+import Model.Organization.Organisation;
+import Model.UserAccount.UserAccount;
+import UI.AdminRole.AdminWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Hp
+ */
+public class AdminRole extends Role{
+    @Override
+    public JPanel workPanel(JPanel userProcessContainer, UserAccount acc, Organisation org, Enterprise ent,Network net, EcoSystem system) {
+        return new AdminWorkAreaJPanel(userProcessContainer,net, ent);
+    }
+    
+     @Override
+    public String toString() {
+        return Type.Admin.getType();
+    }
+
+}
